2f4bd56678617eede9597955a459c4981e289778
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2021-03-03 00:47:09 -0300

    Subida para pruebas

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-    window.addEventListener('scroll', () => {
-        let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        let position = scrollTop * 0.10;
+//     window.addEventListener('scroll', () => {
+//         let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+//         let position = scrollTop * 0.10;
 
-        // console.log(position)
+//         // console.log(position)
 
-        const body = document.querySelector('body');
-        body.style.backgroundPosition = `0 -${position}px`;
-    })
-})
+//         const doc = document.querySelector('html');
+//         doc.style.backgroundPosition = `0 -${position}px`;
+//     })
+// })
